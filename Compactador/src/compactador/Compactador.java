30276e1222942c3e4effa0e45304450183078b45
lucianoarendt <30930778+lucianoarendt@users.noreply.github.com>
2018-06-10 17:21:00 -0300

    Info, Comp

--- Compactador/src/compactador/Compactador.java
+++ Compactador/src/compactador/Compactador.java
@@ -5,40 +5,92 @@
  */
 package compactador;
 
-import arvoreBinariaDeBusca.ArvoreBinariaDeBusca;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import Informacao.Informacao;
+import java.io.*;
+import no.No;
 
 /**
  *
- * @author Leo Minoru
+ * @author Luciano
  */
 public class Compactador {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        try {
-            ArvoreBinariaDeBusca arv = new ArvoreBinariaDeBusca();
-            arv.guarde(25);
+    public static void main(String[] args){
+        // TODO code application logic here
+        try
+        {
+            System.out.println("Digite o endereço do arquivo");
+            BufferedReader tc = new BufferedReader(new InputStreamReader(System.in));
+            String nomeArq = tc.readLine();
+            RandomAccessFile file = new RandomAccessFile(nomeArq, "rw");
             
-            arv.guarde(20);
-            arv.guarde(30);
+            //byte[] byteFile = new byte[Integer.valueOf(new Long(file.length()).toString())];;;
+            //        file.read(byteFile);
+            int[] vetInt = new int[256];
+            for(int i = 0; i < 256; i++)
+                vetInt[i]=0;
+                        
+            for(long i= 0; i < file.length(); i++)              
+                vetInt[file.read()]++;             
             
-            System.out.println(arv.toString());
+            No<Informacao>[] vetNo = new No[256];
             
-            arv.guarde(27);
-            arv.guarde(31);
+            int j = 0;
+            for(int i = 0; i< 256; i++)
+                if(vetInt[i] > 0)
+                {
+                    vetNo[j] = new No(new Informacao(i, vetInt[i]));
+                    j++;
+                }
             
-            System.out.println(arv.toString());
-            
-            arv.excluir(30);
-            
-            System.out.println(arv.toString());
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            int controle = 256;
+            while(controle != 1)
+            {
+                controle = ordenar(vetNo);
+                int novaQtd = vetNo[controle-2].getInfo().getQtd() + vetNo[controle-1].getInfo().getQtd();
+                No<Informacao> novoNoDir = new No(vetNo[controle-1]);
+                No<Informacao> novoNoEsq = new No(vetNo[controle-2]);
+                vetNo[controle-2] = new No(new Informacao(666,novaQtd), novoNoDir, novoNoEsq);
+            }            
         }
+        catch(FileNotFoundException er)
+        {
+            System.err.println("Arquivo não encontrado");
+        }
+        catch(Exception err)
+        {
+            System.err.println(err.getMessage());
+        }
+    }   
+
+    private static int ordenar(No<Informacao>[] vetNo) 
+    {       
+        int lento = 0;
+        while(vetNo[lento] != null)
+        {
+            int oMenor = lento;
+            int rapido = 0;
+            while(vetNo[rapido] != null)
+            {
+                if(vetNo[rapido].getInfo().getQtd() < vetNo[oMenor].getInfo().getQtd())
+                    oMenor = rapido;
+                
+                rapido++;
+            }
+            
+            if(vetNo[oMenor] != vetNo[lento])
+            {
+                No<Informacao> aux = vetNo[oMenor];
+                vetNo[oMenor] = vetNo[lento];
+                vetNo[lento] = aux;
+            }
+
+            lento++;
+        }
+       
+        return lento;
     }
-    
 }
